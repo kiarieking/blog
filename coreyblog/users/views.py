de1aae845d7c20fed2eebaa5c30d484947d542fe
kevin <kiariekevin22@gmail.com>
2020-07-14 21:27:39 +0300

    pagination on blog project

--- coreyblog/users/views.py
+++ coreyblog/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import  UserRegistrationForm, UserUpdateForm, ProfileUpdateForm;
+from .forms import  UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
