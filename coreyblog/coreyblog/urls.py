7ccbc30d357b3edcb7a0a0febcfdc0c7d6b48e9f
kevin <kiariekevin22@gmail.com>
2020-07-23 15:50:31 +0300

    django-heroku settings.py

--- coreyblog/coreyblog/urls.py
+++ coreyblog/coreyblog/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('profile/',user_views.profile, name='profile')
+    path('profile/', user_views.profile, name='profile')
+
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
